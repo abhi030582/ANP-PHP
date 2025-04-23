c8cda637b743a658da1c9e65a1bbe9c6635955b1
abhishekBhardwaj82 <abhishekjee@gmail.com>
2025-04-23 12:36:30 +0530

    pattern program

---
+++ StartPattern1.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1 align='center'>Start Pattern Example</h1>
+<?php
+for ($i = 1; $i <= 3; $i++) {
+    for ($j = 1; $j <= 3; $j++) {
+        echo "* ";
+    }
+    echo "<br>";
+}
+?>
+<h1 align='center'>Second Pattern example</h1>
+<?php
+for ($i = 1; $i <= 5; $i++) {
+    for ($j = 1; $j <= $i; $j++) {
+        echo "* ";
+    }
+    echo "<br>";
+}
+?> 
+<h1 align='center'>Third Start Pattern</h1>
+<?php
+for ($i = 7; $i >= 1; $i--) {
+  
+    for ($k = 1; $k <= 7 - $i; $k++) {
+        echo "&nbsp;&nbsp;";
+    }
+
+    for ($j = 1; $j <= $i; $j++) {
+        echo "* ";
+    }
+    echo "<br>";
+}
+?>
+
+</body>
+</html>
---
+++ forloopExample.php
@@ -0,0 +1,14 @@
+<?php
+for ($i = 1; $i <= 5; $i++) {
+    echo "Number: $i<br>";
+}
+//even numbers
+for ($i = 2; $i <= 10; $i += 2) {
+    echo "Even number: $i<br>";
+}
+
+//reverse loop
+for ($i = 5; $i >= 1; $i--) {
+    echo "Countdown: $i<br>";
+}
+
